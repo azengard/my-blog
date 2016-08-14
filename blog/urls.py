3eb690adeda29816c1e45cb8aaf7e4d420a65f5f
Carlos <quaase-nada@hotmail.com>
2016-08-14 20:40:55 -0300

    add name line at url.py & post_list.html

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 
     url(r'^$', views.post_list),
 
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 
 ]
