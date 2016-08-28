87ea9ca330c905edc4cac23a2471069b241a1e75
Carlos <quaase-nada@hotmail.com>
2016-08-28 14:04:57 -0300

    Add delete post and draft mode on new post before publish

--- blog/views.py
+++ blog/views.py
@@ -34,8 +34,6 @@ def post_new(request):
 
             post.author = request.user
 
-            post.published_date = timezone.now()
-
             post.save()
 
             return redirect('post_detail', pk=post.pk)
@@ -61,8 +59,6 @@ def post_edit(request, pk):
 
             post.author = request.user
 
-            post.published_date = timezone.now()
-
             post.save()
 
             return redirect('post_detail', pk=post.pk)
@@ -72,3 +68,36 @@ def post_edit(request, pk):
             form = PostForm(instance=post)
 
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+
+    post = get_object_or_404(Post, pk=pk)
+
+    post.publish()
+
+    return redirect('blog.views.post_detail', pk=pk)
+
+
+def publish(self):
+
+    self.published_date = timezone.now()
+
+    self.save()
+
+
+def post_remove(request, pk):
+
+    post = get_object_or_404(Post, pk=pk)
+
+    post.delete()
+
+    return redirect('blog.views.post_list')
+
