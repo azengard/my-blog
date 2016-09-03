387f4b8ed3ee5ead30b4ebbbfa4be2ae39634656
Carlos <quaase-nada@hotmail.com>
2016-09-03 18:38:30 -0300

    debugging path url problem

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns =[
+urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 
@@ -30,5 +30,5 @@ urlpatterns =[
 
     url(r'', include('blog.urls')),
 
-]
+)
 
